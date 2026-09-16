54dbafb2c82a61df478a7ec2a794a8381a5f1c87
Ali Esenli <aliriza.esenli@hotmail.com>
2026-09-16 12:50:36 +0200

    feat: excercise 6 microservices

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -15,3 +15,21 @@ output "db_connection_url" {
   value     = "jdbc:postgresql://${digitalocean_database_cluster.postgres.host}:${digitalocean_database_cluster.postgres.port}/${var.db_name}?sslmode=require"
   sensitive = false
 }
+
+output "mysql_host" {
+  value = digitalocean_database_cluster.mysql.host
+}
+
+output "mysql_port" {
+  value = digitalocean_database_cluster.mysql.port
+}
+
+output "mysql_user_password" {
+  value     = digitalocean_database_user.module_app.password
+  sensitive = true
+}
+
+output "mysql_connection_url" {
+  value     = "mysql+pymysql://${digitalocean_database_user.module_app.name}:${digitalocean_database_user.module_app.password}@${digitalocean_database_cluster.mysql.host}:${digitalocean_database_cluster.mysql.port}/${var.mysql_db_name}?charset=utf8mb4"
+  sensitive = true
+}
